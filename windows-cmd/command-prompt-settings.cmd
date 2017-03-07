ddae6ad324957b471b380d3182d5fe09f208f28e
Mayuresh Kadu <mskadu@gmail.com>
2017-03-07 10:52:24 +0000

    Updated command-prompt-settings,cmd: Expanded comment and added tip

--- windows-cmd/command-prompt-settings.cmd
+++ windows-cmd/command-prompt-settings.cmd
@@ -2,8 +2,10 @@
 
 REM File to set up frequently required stuff in a windows command prompt
 REM 
-REM Usage cmd /K %userprofile%\.settings\windows-cmd\command-prompt-settings.cmd
+REM Usage:
+REM   cmd /K %userprofile%\.settings\windows-cmd\command-prompt-settings.cmd
 REM
+REM TIP: Make a shortcut (until I figure out a way to get this to happen automatically)
 
 
 REM Doskey macros
